ed4892654ee028f48c185ca440ec59011c375c05
David Dias <101338327+DavidDias1999@users.noreply.github.com>
2023-01-23 10:49:56 -0300

    index.html estruturado, styles.css estrutura e imagens adicionadas na pasta assets

--- script.js
+++
@@ -1,9 +0,0 @@
-
-$('#criptografar').on("click",adicionarTexto)
-
-function adicionarTexto() {
-    var valorDoComentario = $('#textarea').val()
-
-    $('#mensagemRetorno').text(valorDoComentario)
-}
-
